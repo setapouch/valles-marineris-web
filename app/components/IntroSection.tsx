6b9b7d156035e1d03b53c1e5510f0400d0ca3daa
setapouch <radimjelinek@gmail.com>
2025-11-01 21:04:06 +0100

    Add custom homepage and intro section

---
+++ app/components/IntroSection.tsx
@@ -0,0 +1,13 @@
+export default function IntroSection() {
+  return (
+    <section style={{ marginTop: '2rem', maxWidth: '700px', textAlign: 'center' }}>
+      <h2>Úvod</h2>
+      <p style={{ lineHeight: '1.6' }}>
+        Systém Valles Marineris byl poprvé detailně zmapován sondou Mariner 9 
+        počátkem 70. let. Kaňony dosahují hloubky až 7 km, šířky 200 km a délky přes 4 000 km. 
+        Cílem tohoto projektu je vytvořit interaktivní průvodce, který propojí 
+        vědecká data, vizualizace a 3D modely této úžasné oblasti Marsu.
+      </p>
+    </section>
+  );
+}
